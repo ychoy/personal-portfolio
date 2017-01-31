d05522446e16d991a6dff5db0711ac256369301c
ychoy <yanyinchoy@gmail.com>
2017-01-30 17:10:03 -0800

    fixed javascript and jquery to load data onto portfolio section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,40 +1,45 @@
 // sanity check
 console.log("App.js is connected!");
 
-$(document).on('ready', function(){
-  var data = {
-    "captions": [
-       "Memory Card Game", "Super Rentals", "Portfolio"
-     ],
+$(document).ready(function(){
+  var projects = [
+  {
+		imageAlt: "Memory Card Game screenshot",
+		imageUrl: "https://github.com/ychoy/ychoy.github.io/blob/master/assets/img/memory-game.png?raw=true",
+		captions: "Memory Card Game",
+		description: "I wrote this game using basic Javascript, HTML and CSS. It was fun to take the game to the next level and create a button for players to reset and shuffle a new deck.",
+		githubUrl: "https://github.com/ychoy/wdi-fundamentals-memorygame"
+		},
 
-      "github_url": [
-        "https://github.com/ychoy/wdi-fundamentals-memorygame",
-        "https://github.com/ychoy/super-rentals",
-        "https://github.com/ychoy/ychoy.github.io",
-      ],
+  {
+  imageAlt: "Super Rentals screenshot",
+  imageUrl:   "https://github.com/ychoy/ychoy.github.io/blob/master/assets/img/super%20rentals.png?raw=true",
+  captions: "Super Rentals",
+  description: "I created this web application using Ember.js, a front-end javascript framework. This applciation allows users to search listings by city.",
+  githubUrl: "https://github.com/ychoy/super-rentals"
+  },
 
-      "paragraphInfo": [
-        "I wrote this game using basic Javascript, HTML and CSS. It was fun to take the game to the next level and create a button for players to reset and shuffle a new deck.",
-        "I created this web application using Ember.js, a front-end javascript framework. This applciation allows users to search listings by city.",
-        "I built this portfolio website to showcase my web development projects. It was challenging updating the website to be compatible with Bootstrap and to use jQuery to update the data on the website."
-      ]
-    }
 
-  var images =[
-      "https://github.com/ychoy/ychoy.github.io/blob/master/assets/img/memory-game.png?raw=true",
-      "https://github.com/ychoy/ychoy.github.io/blob/master/assets/img/super%20rentals.png?raw=true",
-      "https://github.com/ychoy/ychoy.github.io/blob/master/assets/img/portfolio.png?raw=true"
-    ]
-
+  {
+  imageAlt: "Portfolio screenshot",
+  imageUrl: "https://github.com/ychoy/ychoy.github.io/blob/master/assets/img/portfolio.png?raw=true",
+  captions: "Portfolio",
+  description: "I built this portfolio website to showcase my web development projects. It was challenging updating the website to be compatible with Bootstrap and to use jQuery to update the data on the website.",
+  githubUrl: "https://github.com/ychoy/ychoy.github.io"
+  },
+]
+/*
   // adding information on each project on the portfolio grid
   for (var i=0; i < data.length; i++) {
     var captions = data.captions[i];
+    console.log(captions);
     $(".caption h3").text(captions);
 
     var eachParagraph = data.paragraphInfo[i];
       $("p#content").text( eachParagraph );
 
     var eachParagraph = "";
+    console.log(eac
 
     var githubUrl = data.github_url[i];
     var link = "<a href='" + githubUrl + "'>" +
@@ -46,4 +51,18 @@ $(document).on('ready', function(){
   for (var i=0; i < images.length; i++) {
     $('.img-responsive').attr('src',images + images[i]);
   };
+
+*/
+  var samples = "";
+  	for (var i = 0; i < projects.length; i++) {
+  		samples += `<div class="col-sm-6 col-md-4">
+                    <img class="img-responsive" src="${projects[i].imageUrl}" alt="${projects[i].imageAlt}"/></a>
+                      <div class="caption">
+                        <h3>${projects[i].captions}</h3>
+                        <p class="content">${projects[i].description}</p>
+                        <p><a href="${projects[i].githubUrl}"class="btn btn-primary" role="button">Learn more</a></p>
+                      </div>
+                  </div>`
+      }
+      $(".portfolio #portfolio .row").append(samples);
 });
