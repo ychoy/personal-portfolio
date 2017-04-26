33a88f93ee1b5af9a9f0ad363b7c9d4fb2be04e4
Yan Yin Choy <yanyinchoy@gmail.com>
2017-04-25 17:13:43 -0700

    restructure sections

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,39 +25,39 @@ $(document).ready(function(){
 		githubUrl: "https://github.com/ychoy/vagabond",
 		liveUrl: "https://levagabond.herokuapp.com"
 		},
-  {
-		imageAlt: "CarpeGov app screenshot",
-		imageUrl: "assets/img/ychoy_carpegov.png",
-		captions: "CarpeGov",
-		description: "Civic engagement tool for administrators to create, read, update, and delete Congressional bills on education and add, show, edit and delete action items taken on the bills. Open source collaboration with Weston Dombroksi. Technologies used: MongoDb, Mongoose, Express, Node.js, Bootstrap, jQuery.",
-		githubUrl: "https://github.com/ychoy/carpe-gov",
-		liveUrl: "https://carpegov.herokuapp.com"
-		},
-
-  {
-  imageAlt: "Spotify Search app screenshot",
-  imageUrl:   "assets/img/ychoy_spotifysearch.png",
-  captions: "Spotify Search",
-  description: "Web application that allows users to search for songs on the Spotify API. Technologies used include: jQuery, AJAX, Bootstrap, Spotify API",
-  githubUrl: "https://github.com/ychoy/spotify-search-lab",
-  liveUrl: "https://ychoy.github.io/spotify-search-lab/"
-  },
-
-
-  {
-  imageAlt: "Starship Racing Game screenshot",
-  imageUrl: "assets/img/ychoy_racinggame.png",
-  captions: "Starship Racing Game",
-  description: "Two players can operate starships to race to the final frontier. Technologies used include: HTML, CSS, Bootstrap, jQuery",
-  githubUrl: "https://github.com/ychoy/racing-game",
-  liveUrl: "https://ychoy.github.io/racing-game/"
-  },
+  // {
+	// 	imageAlt: "CarpeGov app screenshot",
+	// 	imageUrl: "assets/img/ychoy_carpegov.png",
+	// 	captions: "CarpeGov",
+	// 	description: "Civic engagement tool for administrators to create, read, update, and delete Congressional bills on education and add, show, edit and delete action items taken on the bills. Open source collaboration with Weston Dombroksi. Technologies used: MongoDb, Mongoose, Express, Node.js, Bootstrap, jQuery.",
+	// 	githubUrl: "https://github.com/ychoy/carpe-gov",
+	// 	liveUrl: "https://carpegov.herokuapp.com"
+	// 	},
+  //
+  // {
+  // imageAlt: "Spotify Search app screenshot",
+  // imageUrl:   "assets/img/ychoy_spotifysearch.png",
+  // captions: "Spotify Search",
+  // description: "Web application that allows users to search for songs on the Spotify API. Technologies used include: jQuery, AJAX, Bootstrap, Spotify API",
+  // githubUrl: "https://github.com/ychoy/spotify-search-lab",
+  // liveUrl: "https://ychoy.github.io/spotify-search-lab/"
+  // },
+  //
+  //
+  // {
+  // imageAlt: "Starship Racing Game screenshot",
+  // imageUrl: "assets/img/ychoy_racinggame.png",
+  // captions: "Starship Racing Game",
+  // description: "Two players can operate starships to race to the final frontier. Technologies used include: HTML, CSS, Bootstrap, jQuery",
+  // githubUrl: "https://github.com/ychoy/racing-game",
+  // liveUrl: "https://ychoy.github.io/racing-game/"
+  // },
 ]
 
   var samples = "";
   	for (var i = 0; i < projects.length; i++) {
   		samples += `
-  		  <div class="col-sm-6 col-md-4">
+  		  <div class="col-md-12">
           <img class="img-responsive" src="${projects[i].imageUrl}" alt="${projects[i].imageAlt}"/></a>
             <div class="caption">
               <h3>${projects[i].captions}</h3>
