9bce9f5d98954b657f594edde62bf7e2e8cb4e1f
Yan Yin Choy <yanyinchoy@gmail.com>
2017-06-23 07:48:05 -0700

    add roles

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,21 +7,23 @@ $(document).ready(function(){
 		captions: "Rydeshare",
 		description: "Create, join and search for carpools to save time and money and reduce stress on the commute  ",
     about: "I ideated, designed, and built RESTful CRUD functionality for users to create and join carpools. I developed a search feature that queries Google Maps API and displays markers on a dynamic map.",
-		projectType: "I was the designer and developer",
+		role: "Full Stack Developer & Designer",
+    projectType: "Solo Project",
     tech: "Rails・Google Maps API・Geocoder・PostgreSQL・HTML・CSS・jQuery・Devise・Semantic UI SASS",
     githubUrl: "https://github.com/ychoy/rydeshare",
 		liveUrl: "https://rydeshare.herokuapp.com"
 		},
   {
-      imageALt: "Find A Home",
-      imageUrl: "assets/img/findahome_may2017_thumbnail.png",
-      captions: "Find A Home",
-      description: "Find affordable housing near you",
-      about: "I consulted local government partner to obtain data on affordable housing listings. I collaborated with Tyler to code web scraping algorithms in Ruby to parse data. I recruited developers and collaborated to craft wireframes and roadmap. I updated schema for data model in PostgreSQL. I developed and designed index view and map markers display for all listings. I collaborated with the team to conduct user research.",
-      projectType: "Open source collaboration built with developers - Tyler Hampton, Angelique de Castro, Sanaz Jamloo, and UX/UI designer - Julie Kim",
-      tech: "Rails・Ember・Node・PostgreSQL・Redis・Google Maps API・Geocoder・Semantic UI SASS",
-      githubUrl: "http://github.com/codeforsanjose/findahome",
-      liveUrl: "https://github.com/codeforsanjose/findahome"
+    imageALt: "Find A Home",
+    imageUrl: "assets/img/findahome_may2017_thumbnail.png",
+    captions: "Find A Home",
+    description: "Find affordable housing near you",
+    about: "I consulted local government partner to obtain data on affordable housing listings. I collaborated with Tyler to code web scraping algorithms in Ruby to parse data. I recruited developers and collaborated to craft wireframes and roadmap. I updated schema for data model in PostgreSQL. I developed and designed index view and map markers display for all listings. I collaborated with the team to conduct user research.",
+    role: "Full Stack Developer, Project Manager, UX Researcher",
+    projectType: "Open source collaboration built with developers - Tyler Hampton, Angelique de Castro, Sanaz Jamloo, and UX/UI designer - Julie Kim",
+    tech: "Rails・Ember・Node・PostgreSQL・Redis・Google Maps API・Geocoder・Semantic UI SASS",
+    githubUrl: "http://github.com/codeforsanjose/findahome",
+    liveUrl: "https://github.com/codeforsanjose/findahome"
     },
 
  {
@@ -30,6 +32,7 @@ $(document).ready(function(){
 		captions: "WeHuddle",
 		description: "Chat in real-time and empower your teams to collaborate more  ",
     about: "I developed the direct messaging feature, performed integration tests, and refactored code for OOP design. I also drafted user stories and sprint specifications, and designed chatroom sidebar and forms. I collaborated with Thelma and Will to persist two models with Active Record and PostgreSQL and develop the Chatroom feature and user login.",
+    role: "Full Stack Developer",
     projectType: "Open source collaboration built with Thelma Boamah and Will Kaspar. Consulted UX/UI designer Luke Swanson.",
     tech: "HTML・CSS・jQuery・Semantic UI・ Rails・Action Cable・WebSockets・Devise・Carrierwave・PostgreSQL・rSpec Rails",
 		githubUrl: "https://github.com/ychoy/wehuddle",
@@ -41,6 +44,7 @@ $(document).ready(function(){
 		captions: "Le Vagabond",
 		description: "Create and share hints on your favorite cities  ",
     about: "I built CRUD functionality for users to create, edit, delete comments to travel hints. I also developed the main dashboard page, login and user authentication page. I also consulted UX/UI designers and implemented advice for responsive front end design. Adjusted and implemented features based on client feedback",
+    role: "Full Stack Developer",
     projectType: "Open source collaboration built with Christen Williams and Auston Wilkinson. Consulted UX/UI designers Daniel Peck, Katie Yi, Seungjae Yun, Justin Wong, Harry Byres, and Urvi Jain.",
     tech: "HTML・CSS・Bootstrap・Rails・PostgreSQL",
     githubUrl: "https://github.com/ychoy/vagabond",
@@ -52,7 +56,8 @@ $(document).ready(function(){
 	  captions: "CarpeGov",
 	  description: "Track Congressional bills on education and add and track actions taken on those bills.",
 	  about: "I built CRUD functionality for users to create, edit, update and delete action items, persisted two models with MongoDB, and styled the frontend with Bootstrap. I collaborated with Weston to craft wireframes, ERDs and user stories.",
-	  projectType: "Open source collaboration built with Weston Dombroski",
+    role: "Full Stack Developer",
+    projectType: "Open source collaboration built with Weston Dombroski",
 	  tech: "MongoDB・Mongoose・Express・Node.js・Bootstrap・HTML・CSS・jQuery",
 	  githubUrl: "https://github.com/ychoy/carpe-gov",
 	  liveUrl: "https://carpegov.herokuapp.com"
@@ -72,10 +77,11 @@ $(document).ready(function(){
           <div class="col-sm-6 col-md-8">
             <br>
             <h3><a href="${projects[i].liveUrl}">${projects[i].captions}</a> <a href="${projects[i].githubUrl}"><img src="assets/img/github.png" alt="Github" class="social-icon"></a></h3>
-            <p><em>${projects[i].description}</em></p>
-            <p>${projects[i].about}</p>
+            <p><strong>${projects[i].description}</strong></p>
+            <p><strong>Description: </strong>${projects[i].about}</p>
+            <p><strong>Role: </strong>${projects[i].role}</p>
             <p><strong>Team: </strong>${projects[i].projectType}</p>
-            <p>${projects[i].tech}</p>
+            <p><strong>Technologies: </strong>${projects[i].tech}</p>
           </div>
         </div>
         <br>
