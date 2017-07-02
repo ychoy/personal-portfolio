6bd1b6ef3c2bd47238c06b5c754c07101cb842d0
Yan Yin Choy <yanyinchoy@gmail.com>
2017-07-01 20:34:22 -0700

    updated description

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 		imageUrl: "assets/img/ychoy_rydeshare.png",
 		captions: "Rydeshare",
 		description: "Create, join and search for carpools to save time and money and reduce stress on the commute  ",
-    about: "Inspired by the long commute I took from San José to San Francisco to General Assembly, I wanted to build a responsive web application that empowered people to reduce their stress, time and money spent on their commute. I designed and developed Rydeshare, a web application that allows users to create accounts, create carpool rides as a driver, join other drivers' carpools, and filter carpool departures on a map. I built CRUD fundtionality for users to create, edit, and remove their carpools. I also created CRUD functionality for users to join and leave carpools. I developed a search feature that queries Google Maps API and displays markers on a dynamic map with Google Maps API and Gmaps.js.",
+    about: "Inspired by the long commute from San José to San Francisco, I wanted to build a responsive web application that empowered people to reduce their stress, time and money spent on their commute. I designed and developed Rydeshare, a web application that allows users to create accounts, create carpool rides as a driver, join other drivers' carpools, and filter carpool departures on a map. I built CRUD fundtionality for users to create, edit, and remove their carpools. I also created CRUD functionality for users to join and leave carpools. I developed a search feature that queries Google Maps API and displays markers on a dynamic map.",
 		role: "Full Stack Developer & Designer",
     projectType: "Solo Project",
     tech: "Rails・Google Maps API・Geocoder・PostgreSQL・HTML・CSS・jQuery・Devise・Semantic UI SASS・Heroku",
@@ -46,7 +46,7 @@ $(document).ready(function(){
     about: "We built Le Vagabond for a client who wanted a responsive web application that allowed users to share hints about favorite cities throughout their travels. I collaborated with Christen Williams and Auston Wilkinson to build this web app. I developed the CRUD functionality for users to create, edit, delete comments to travel hints. I also developed the main dashboard page and user login page and incorporated user authentication. We consulted UX/UI designers Daniel Peck, Katie Yi, Seungjae Yun, Justin Wong, Harry Byres, and Urvi Jain for feedback on improving user interface. Based on feedback from our client and designers, we adjusted features to enrich user experience.",
     role: "Full Stack Developer",
     projectType: "Team Project",
-    tech: "HTML・CSS・Bootstrap・Rails・PostgreSQL・",
+    tech: "HTML・CSS・Bootstrap・Rails・PostgreSQL・ Heroku",
     githubUrl: "https://github.com/ychoy/vagabond",
 		liveUrl: "https://levagabond.herokuapp.com"
 		},
@@ -68,15 +68,13 @@ $(document).ready(function(){
   	for (var i = 0; i < projects.length; i++) {
   		samples += `
         <div class="row">
-  		    <div class="col-sm-6 col-md-4">
-          <br>
+  		    <div class="col-sm-6 col-md-6">
           <br>
             <div class="thumbnail">
               <a href="${projects[i].liveUrl}"><img class="img-responsive" src="${projects[i].imageUrl}" alt="${projects[i].imageAlt}"/></a>
             </div>
           </div>
-          <div class="col-sm-6 col-md-8">
-            <br>
+          <div class="col-sm-6 col-md-6">
             <h3><a href="${projects[i].liveUrl}">${projects[i].captions}</a> <a href="${projects[i].githubUrl}"><img src="assets/img/github.png" alt="Github" class="social-icon"></a></h3>
             <h4 style="color: darkcyan">${projects[i].description}</h4>
             <p>${projects[i].about}</p>
