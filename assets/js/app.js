3427fd9db0dadd88f5b280772fd894121241195e
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-11-06 14:04:36 -0800

    update projects

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,18 +2,6 @@ $(document).ready(function(){
 
   var projects = [
   {
-		imageAlt: "Rydeshare app screenshot",
-		imageUrl: "assets/img/ychoy_rydeshare.png",
-		captions: "Rydeshare",
-		description: "Create, join and search for carpools to save time and money and reduce stress on the commute  ",
-    about: "I was inspired to build Rydeshare because of my long commute from San José to San Francisco, which was often affected by public transit issues or traffic. Rydeshare allows users to create accounts, create carpool rides as a driver, join other drivers' carpools, and filter carpool departures on a map. I built CRUD fundtionality for users to create, edit, and remove their carpools. I also created CRUD functionality for users to join and leave carpools. I developed a search feature that queries Google Maps API and displays markers on a dynamic map.",
-		role: "Full Stack Developer & Designer",
-    projectType: "Solo Project",
-    tech: "Rails・Google Maps API・Geocoder・PostgreSQL・HTML・CSS・jQuery・Devise・Semantic UI SASS・Heroku",
-    githubUrl: "https://github.com/ychoy/rydeshare",
-		liveUrl: "https://rydeshare.herokuapp.com"
-		},
-    {
     imageAlt: "Renter's Rights Guide",
     imageUrl: "assets/img/CFSJ_rentersrightsguide_app.png",
     captions: "Renter's Rights Guide",
@@ -25,7 +13,31 @@ $(document).ready(function(){
     githubUrl: "https://github.com/codeforsanjose/renters-rights",
     liveUrl: "https://renters-rights.herokuapp.com/"  
     },
- /* {
+  {
+    imageAlt: "Choose Your Win",
+    imageUrl: "assets/img/ChooseYourWin.png",
+    captions: "Choose Your Win",
+    description: "Navigating issues that affect women in the workplace through an interactive game",
+    about: "I collaborated with friends at Hack the Patriarchy to design and create this interactive game that allows users to navigate issues that affect women in the workplace, beginning with the hiring process. I contributed to user stories, conducted research with a subject matter expert from the Women's Bureau, and designed and coded the gameplay pages with JavaScript, HTML and CSS. I worked with Thelma Boamah, Emily Ramos, Maliha Ahmed and Lorin Camargo. Our team won 'Most Original Project'! ", 
+    role: "Front End Developer",
+    projectType: "Team Project",
+    tech: "JavaScript・HTML・CSS",
+    githubUrl: "https://github.com/ychoy/chooseyourwin",
+    liveUrl: "https://yanyinchoy.com/chooseyourwin/"  
+    },
+  {
+		imageAlt: "Rydeshare app screenshot",
+		imageUrl: "assets/img/ychoy_rydeshare.png",
+		captions: "Rydeshare",
+		description: "Create, join and search for carpools to save time and money and reduce stress on the commute  ",
+    about: "I was inspired to build Rydeshare because of my long commute from San José to San Francisco, which was often affected by public transit issues or traffic. Rydeshare allows users to create accounts, create carpool rides as a driver, join other drivers' carpools, and filter carpool departures on a map. I built CRUD fundtionality for users to create, edit, and remove their carpools. I also created CRUD functionality for users to join and leave carpools. I developed a search feature that queries Google Maps API and displays markers on a dynamic map.",
+		role: "Full Stack Developer & Designer",
+    projectType: "Solo Project",
+    tech: "Rails・Google Maps API・Geocoder・PostgreSQL・HTML・CSS・jQuery・Devise・Semantic UI SASS・Heroku",
+    githubUrl: "https://github.com/ychoy/rydeshare",
+		liveUrl: "https://rydeshare.herokuapp.com"
+		},
+/* {
     imageALt: "Find A Home",
     imageUrl: "assets/img/findahome_may2017_thumbnail.png",
     captions: "Find A Home",
@@ -74,98 +86,30 @@ $(document).ready(function(){
 	  githubUrl: "https://github.com/ychoy/carpe-gov",
 	  liveUrl: "https://carpegov.herokuapp.com"
 	  }
+
 ]
 
   var samples = "";
 
-	for (var i = 0; i < 3; i++) {
+	for (var i = 0; i < projects.length; i++) {
 		samples += `
       <div class="row">
 		    <div class="col-sm-6 col-md-6">
-          <br>
-          <div class="caption">
-            <h3><a href="${projects[i].liveUrl}">${projects[i].captions}</a> </h3>
-          </div>
           <div class="thumbnail">
             <a href="${projects[i].liveUrl}"><img class="img-responsive" src="${projects[i].imageUrl}" alt="${projects[i].imageAlt}"/></a>
           </div>
         </div>
         <div class="col-sm-6 col-md-6">
-          <br>
-          <br>
-          <br>
+          <h3><a href="${projects[i].liveUrl}">${projects[i].captions}</a> </h3>
           <h4 style: "color: darkcyan">${projects[i].description}</h4>
-          <p>${projects[i].about}</p>
-          <p>Visit <a href="${projects[i].githubUrl}"><strong>Github</strong></a></p>
-          <p><strong>Role: </strong>${projects[i].role}</p>
-          <p><strong>Team: </strong>${projects[i].projectType}</p>
-          <p><strong>Tools: </strong>${projects[i].tech}</p>
+          <p>${projects[i].about} <a href="${projects[i].githubUrl}"><i class="fa fa-github" aria-hidden="true"></i></a></p>
+          <p><i class="fa fa-user" aria-hidden="true"></i> ${projects[i].role}, ${projects[i].projectType}</p>
+          <p><i class="fa fa-wrench" aria-hidden="true"></i> ${projects[i].tech}</p>
         </div>
       </div>
       <br>
       `
     }
-
-    samples += `
-      <div class="row">
-  		    <div class="col-sm-6 col-md-6">
-            <br>
-            <div class="caption">
-              <h3><a href="http://girlslearn.org/international-partner-schools/">Girls Learn International</a> </h3>
-            </div>
-            <div class="thumbnail">
-              <iframe class="embed-responsive-item" width="540" height="315" src="https://www.youtube.com/embed/t8rL7eyVt8s" frameborder="0" allowfullscreen></iframe>
-            </div>
-          </div>
-          <div class="col-sm-6 col-md-6">
-            <br>
-            <br>
-            <br>
-            <h4 style: "color: darkcyan">Create an Appealing Call-to-Action Partner Network webpage</h4>
-            <p>Girls Learn International (GLI) is an non-profit that empowers American schools to fundraise to help girls stay in school abroad.
-            GLI engages middle school and high school students and faculty to fundraise for supplies and classroom improvements, health and clean water and technology, arts and media.
-              At the Hactivists Unite hackathon in July 2017, GLI asked us to solve the following problems with their Partner Network page:
-              <ul>
-                <li> Webpage is dense with information and not appealing </li>
-                <li> Message is not succinct</li>
-                <li> Call to action is not clear</li>
-                <li> No existing forms for project-specific online donation</li>
-                </ul>
-            <p>We discussed features that would transform the <a href="assets/img/original_gliwebpage.png">original webpage</a> into a more dynamic and appealing user experience.
-              Based on Jemma and James' <a href="assets/img/gli_wireframes.png">wireframes</a>, redesigned the webpage to improve UX/UI with WordPress and deployed a custom plugin.
-              I also designed UI for the donation form using Salsa Labs and Bootstrap.</p>
-            <p><strong>Role: </strong>Front End Developer</p>
-            <p><strong>Team: </strong> UX/UI Designers: <a href="https://www.jemmapearce-ux.com/">Jemma Pearce</a> and James Kaguyutan, Nonprofit Partners: Ashley Steimer-King and Carmen Rios </p>
-            <p><strong>Tools: </strong> WordPress・HTML・CSS・Bootstrap</p>
-          </div>
-        </div>`
-      for (var i = 3; i < projects.length; i++) {
-    		samples += `
-          <div class="row">
-    		    <div class="col-sm-6 col-md-6">
-              <br>
-              <div class="caption">
-                <h3><a href="${projects[i].liveUrl}" target="_blank">${projects[i].captions}</a> </h3>
-              </div>
-              <div class="thumbnail">
-                <a href="${projects[i].liveUrl}" target="_blank"><img class="img-responsive" src="${projects[i].imageUrl}" alt="${projects[i].imageAlt}"/></a>
-              </div>
-            </div>
-            <div class="col-sm-6 col-md-6">
-              <br>
-              <br>
-              <br>
-              <h4 style: "color: darkcyan">${projects[i].description}</h4>
-              <p>${projects[i].about}</p>
-              <p>Visit <a href="${projects[i].githubUrl}" target="_blank"><strong>Github</strong></a></p>
-              <p><strong>Role: </strong>${projects[i].role}</p>
-              <p><strong>Team: </strong>${projects[i].projectType}</p>
-              <p><strong>Tools: </strong>${projects[i].tech}</p>
-            </div>
-          </div>
-          <br>
-          `
-        }
 
     $(".portfolio #portfolio").append(samples);
 
