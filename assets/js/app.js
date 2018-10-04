47dffb60ed2f5fabaa21af31858a8ba663a9c3ec
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-10-04 10:12:09 -0700

    add info about new project

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,16 @@ $(document).ready(function(){
     liveUrl: "http://beta.descartae.com"  
     },
   {
+    imageAlt: "YYZ Engine - Hello World", 
+    imageUrl: "assets/img/YYZEngine-HelloWorld-CustomAnimationScreenshot.png",
+    captions: "YYZ Engine - Hello World",
+    description: "Custom animations based on custom greeting and local date and time",
+    about: "I built an API for a custom greeting based on local date and time, and the user's country. I also created a custom animation that displays an animated sun and sky depending on the custom greeting and local time and date. I built this project to learn how to set up continuous integration, coverage reporting and Docker. I also wanted to improve my skills in REST API development and TDD.",
+    role: "Software Engineer",
+    productType: "Solo Project",
+    tech: "Node.js ・Express ・React ・Docker ・Travis CI ・Coveralls ・Jest ・Jasmine ・ip2countrify ・public-ip"
+  },
+  {
     imageAlt: "Renter's Rights Guide",
     imageUrl: "assets/img/CFSJ_rentersrightsguide_app.png",
     captions: "Renter's Rights Guide",
